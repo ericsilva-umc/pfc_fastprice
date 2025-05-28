763dbd255209cf52a284de57903cc88c47d12835
ericsilva-umc <11212100793@alunos.umc.br>
2025-05-28 16:43:49 -0300

    Delete pfc_fastprice directory

--- pfc_fastprice/src/main/java/com/umc/pfc_fastprice/repository/SugestaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.umc.pfc_fastprice.repository;
-
-import com.umc.pfc_fastprice.model.Sugestao;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SugestaoRepository extends MongoRepository<Sugestao, String> {
-}
